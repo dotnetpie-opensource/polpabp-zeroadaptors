2ad13d11276f4548d51d9cc3a740379acfe5c7cd
dotnetpie <polpabp@gmail.com>
2022-02-23 14:44:28 -0500

    Org unit permissions

---
+++ src/PolpAbp.ZeroAdaptors.Application.Contracts/OrganizationUnitPermissionDefinitionProvider.cs
@@ -0,0 +1,26 @@
+﻿using PolpAbp.ZeroAdaptors.Authorization;
+using PolpAbp.ZeroAdaptors.Localization.Organizations;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.Localization;
+
+namespace PolpAbp.ZeroAdaptors
+{
+    public class OrganizationUnitPermissionDefinitionProvider : PermissionDefinitionProvider
+    {
+        public override void Define(IPermissionDefinitionContext context)
+        {
+            var identityGroup = context.AddGroup(OrganizationUnitPermissions.GroupName, L("Permission:OrganizationUnits"));
+
+            var rolesPermission = identityGroup.AddPermission(OrganizationUnitPermissions.Default, L("Permission:OrganizationUnits_Default"));
+            rolesPermission.AddChild(OrganizationUnitPermissions.ManageTree, L("Permission:OrganizationUnits_ManageOrganizationTree"));
+            rolesPermission.AddChild(OrganizationUnitPermissions.ManageMembers, L("Permission:OrganizationUnits_ManageMembers"));
+            rolesPermission.AddChild(OrganizationUnitPermissions.ManageRoles, L("Permission:OrganizationUnits_ManageRoles"));
+            rolesPermission.AddChild(OrganizationUnitPermissions.ManagePermissions, L("Permission:OrganizationUnits_ManagePermissions"));
+        }
+
+        private static LocalizableString L(string name)
+        {
+            return LocalizableString.Create<ZeroAdaptorsOrganizationsResource>(name);
+        }
+    }
+}
