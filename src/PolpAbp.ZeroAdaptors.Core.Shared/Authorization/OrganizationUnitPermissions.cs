3b59da2d1124431f809ba7a03b58232481444b28
dotnetpie <polpabp@gmail.com>
2022-02-23 14:16:36 -0500

    Organization unit app service

---
+++ src/PolpAbp.ZeroAdaptors.Core.Shared/Authorization/OrganizationUnitPermissions.cs
@@ -0,0 +1,23 @@
+﻿using Volo.Abp.Reflection;
+
+namespace PolpAbp.ZeroAdaptors.Authorization
+{
+    /// <summary>
+    /// Defines the list of permissions applying to organization units.
+    /// </summary>
+    public static class OrganizationUnitPermissions
+    {
+        private const string GroupName = "PolpAbp";
+
+        public const string Default = GroupName + ".OrganizationUnits";
+        public const string ManageTree = Default + ".ManageOrganizationTree";
+        public const string ManageMembers = Default + ".ManageMembers";
+        public const string ManageRoles = Default + ".ManageRoles";
+        public const string ManagePermissions = Default + "ManagePermissions";
+
+        public static string[] GetAll()
+        {
+            return ReflectionHelper.GetPublicConstantsRecursively(typeof(OrganizationUnitPermissions));
+        }
+    }
+}
