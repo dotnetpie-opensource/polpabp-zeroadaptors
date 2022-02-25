3ad116999a8c19e8fc4a56dd4b55e66b5c7955d8
dotnetpie <polpabp@gmail.com>
2022-02-25 05:20:02 -0500

    Permission convertor

---
+++ src/PolpAbp.ZeroAdaptors.Application.Contracts/Authorization/Permissions/IPermissionConvertor.cs
@@ -0,0 +1,30 @@
+﻿namespace PolpAbp.ZeroAdaptors.Authorization.Permissions
+{
+    /// <summary>
+    /// Provides the permission transformations 
+    /// across the application. 
+    /// By defining an applicaiton specific implementation, 
+    /// the application may have its own expected permission 
+    /// names.
+    /// The packages include a identity mappping function 
+    /// as the default implementation.
+    /// </summary>
+    public interface IPermissionConvertor
+    {
+        /// <summary>
+        /// Converts the given application-specific permission name 
+        /// into a common one from a library
+        /// </summary>
+        /// <param name="input">Application-specific permission name</param>
+        /// <returns>Permission name</returns>
+        string Decode(string input);
+
+        /// <summary>
+        /// Converts the given permission name into 
+        /// an application-specific one.
+        /// </summary>
+        /// <param name="input">Permission name</param>
+        /// <returns>Application-specific permission name</returns>
+        string Encode(string input);
+    }
+}
---
+++ src/PolpAbp.ZeroAdaptors.Application/Authorization/Permissions/TrivialPermissionConvertor.cs
@@ -0,0 +1,20 @@
+﻿using Volo.Abp.DependencyInjection;
+
+namespace PolpAbp.ZeroAdaptors.Authorization.Permissions
+{
+    /// <summary>
+    /// Provides the default implementation (identity mapping) for IPermissionConvertor.
+    /// </summary>
+    public class TrivialPermissionConvertor : IPermissionConvertor, ISingletonDependency
+    {
+        public string Encode(string input)
+        {
+            return input;
+        }
+
+        public string Decode(string input)
+        {
+            return input;
+        }
+    }
+}
