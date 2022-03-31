162ebde30c013069bc9255ac3784704da3c8b789
dotnetpie <polpabp@gmail.com>
2022-03-31 04:55:08 -0400

    Missed files

---
+++ src/PolpAbp.ZeroAdaptors.Application.Contracts/Authorization/Roles/Dto/UsersToRoleInput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PolpAbp.ZeroAdaptors.Authorization.Roles.Dto
+{
+    public class UsersToRoleInput
+    {
+        public Guid[] UserIds { get; set; }
+
+        public Guid RoleId { get; set; }
+    }
+}
