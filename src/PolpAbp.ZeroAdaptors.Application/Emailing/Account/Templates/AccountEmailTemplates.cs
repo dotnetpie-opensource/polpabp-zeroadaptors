a00a3e3b86e43968520659b2a442fc5222e020d7
Tom Tang <polpware@gmail.com>
2022-09-16 04:09:28 -0400

    Clean up as some code is refactored into the framework

--- src/PolpAbp.ZeroAdaptors.Application/Emailing/Account/Templates/AccountEmailTemplates.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PolpAbp.ZeroAdaptors.Emailing.Account.Templates
-{
-    public static class AccountEmailTemplates
-    {
-        public const string EmailActivationtLink = "PolpAbp.Account.EmailActivationLink";
-
-    }
-}
